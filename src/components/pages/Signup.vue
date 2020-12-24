66c77d04f062637e074d748a5094c741e7791fcf
dhruw <dhruwlalan22@gmail.com>
2020-12-24 22:00:44 +0530

    add: back-link comp

--- src/components/pages/Signup.vue
+++ src/components/pages/Signup.vue
@@ -1,24 +1,7 @@
 <template>
    <form class="form form--signup" autocomplete="off">
       <div class="form__header">
-         <router-link :to="{ name: 'home' }" tabindex="-1"
-            ><svg
-               class="form__header--back"
-               xmlns="http://www.w3.org/2000/svg"
-               xmlns:xlink="http://www.w3.org/1999/xlink"
-               aria-hidden="true"
-               focusable="false"
-               role="img"
-               width="1em"
-               height="1em"
-               preserveAspectRatio="xMidYMid meet"
-               viewBox="0 0 512 512"
-               style="transform: rotate(360deg)"
-            >
-               <path
-                  d="M401.4 224h-214l83-79.4c11.9-12.5 11.9-32.7 0-45.2s-31.2-12.5-43.2 0L89 233.4c-6 5.8-9 13.7-9 22.4v.4c0 8.7 3 16.6 9 22.4l138.1 134c12 12.5 31.3 12.5 43.2 0 11.9-12.5 11.9-32.7 0-45.2l-83-79.4h214c16.9 0 30.6-14.3 30.6-32 .1-18-13.6-32-30.5-32z"
-               ></path></svg
-         ></router-link>
+         <back-link name="home"></back-link>
          <h3 class="form__header--title">Sign Up</h3>
       </div>
       <div class="form__body">
@@ -62,3 +45,13 @@
       </svg>
    </form>
 </template>
+
+<script>
+import BackLink from '../utils/BackLink.vue';
+
+export default {
+   components: {
+      BackLink,
+   },
+};
+</script>
