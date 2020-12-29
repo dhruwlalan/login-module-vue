1eb144b937cb408032c32fca90fc1829b3cf6b4a
dhruw <dhruwlalan22@gmail.com>
2020-12-29 15:50:58 +0530

    seperated firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/storage';
-
-///Your web app's Firebase configuration///
-const firebaseConfig = {
-   apiKey: 'AIzaSyBnv8pvyLFPizxhL7GZhZjY1CV4go-Wbfg',
-   authDomain: 'login-module-vue.firebaseapp.com',
-   projectId: 'login-module-vue',
-   storageBucket: 'login-module-vue.appspot.com',
-   messagingSenderId: '338708981633',
-   appId: '1:338708981633:web:e4fcc748367361607cea80',
-};
+import firebaseConfig from '../firebaseConfig';
 
 ///Initialize Firebase///
 firebase.initializeApp(firebaseConfig);
 
-///utils///
+///Utils///
 const fb = firebase;
 const auth = firebase.auth();
 const storageRef = firebase.storage().ref();
